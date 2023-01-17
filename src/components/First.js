7c068251e5541d4d889a8678ac32d3dd13918fbe
김민수 <min.su@daum.net>
2023-01-18 00:27:17 +0900

    데이터 뿌리기 연습(성공)

--- src/components/First.js
+++ src/components/First.js
@@ -1,14 +1,13 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { type } from "@testing-library/user-event/dist/type";
 
 function First() {
-  const [hello, setHello] = useState();
-  const [db, setDb] = useState();
+  const [hello, setHello] = useState('');
+  const [data, setData] = useState([]);
 
   // useEffect(() => {
   //   axios
-  //     .get("/api/hello")
+  //     .get("/hello")
   //     .then((response) => setHello(response.data))
   //     .catch((error) => console.log(error));
   // }, []);
@@ -16,24 +15,63 @@ function First() {
   useEffect(() => {
     axios
       .get("/data")
-      .then((response) => setDb(response.data))
+      .then((response) => setData(response.data))
       .catch((error) => console.log(error));
   }, []);
 
-    let dblist = JSON.stringify(db);
-    
-    const dblist1 = JSON.parse()
 
-  //   console.log(typeof dblist);
+  let db = JSON.stringify(data)
+  // console.log(data)
 
-  //   let dbArr = Object.keys(dblist);
-  //   console.log(typeof dblist);
+  //화면에 출력할 정보를 담을 오브젝트 생성
+  const List = {};
+  //필요한 키값 불러오기
+  const key1 = ["car_num", "distance", "distance_cum", "speed_av", "speed_max",
+    "opertating_time", "stop_num", "stop_time", "stop_rate", "drive_time",
+    "rpm_max", "rpm_av", "result"];
+
+  //key에 해당하는 value 추출
+  for (let k of key1) {
+    List[k] = db[k];
+  }
+
+  // console.log(List)
+
+  // console.log(typeof (data))  //obj
+  // console.log(List)
+
+  //화면에 출력할 내용 jsx로 만들기
+  let View = [];
+
+  for (let [k, v] of Object.entries(List)) {
+    View.push(
+      <li key={List.car_num}>
+        <span>{k}</span>
+        <span>{v}</span>
+      </li>
+    )
+  }
+  useEffect(() => { }, []);
 
   return (
     <>
-      <div>백엔드에서 가져온 데이터입니다 : {dblist}</div>
+      {/* <div>백엔드에서 가져온 데이터입니다 : {db}</div> */}
+      {/* {View} */}
+
+      {data
+        ? data.map((props) => {
+          return (
+            <ul>
+              <li>차번호 : {props.car_num} 이동거리 :{props.distance}km</li>
+            </ul>
+          )
+        })
+        : null
+      }
+
     </>
   );
 }
 
 export default First;
+
