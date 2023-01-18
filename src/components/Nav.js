00c9fe079c233237aea668c2a26b8952dc22f5dd
Minsu0207 <minsu.880207@gmail.com>
2023-01-18 12:52:03 +0900

    음

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,22 +1,23 @@
 import { useState } from "react";
 
-
 function Nav() {
-  let [name, names] = useState(["소주제1", "소주제2", "소주제3"])
-  
+  let [name, names] = useState(["소주제1", "소주제2", "소주제3"]);
 
   return (
     <>
-        
       <h2>components/Nav</h2>
+      <h3>음</h3>
       <ul>
-        <li className="li1" onClick={
-          () => {
+        <li
+          className="li1"
+          onClick={() => {
             let sub1 = { ...name };
-            sub1[0]="운전자별 인지능력 평가"
-            names(sub1)
-           }
-        }>{name[0]}</li>
+            sub1[0] = "운전자별 인지능력 평가";
+            names(sub1);
+          }}
+        >
+          {name[0]}
+        </li>
         <li className="li2">{name[1]}</li>
         <li className="li3">{name[2]}</li>
       </ul>
