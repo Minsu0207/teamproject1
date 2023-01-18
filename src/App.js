e52414fb1e81884f44f170fabba67163ea0a18cf
Minsu0207 <minsu.880207@gmail.com>
2023-01-18 18:15:35 +0900

    퇴근!

--- src/App.js
+++ src/App.js
@@ -2,22 +2,17 @@ import logo from "./logo.svg";
 import "./App.css";
 import React, { useState, useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
-import Main from "../src/components/Main";
+import Main from "./components/Main";
 import Page1 from "./components/Page1";
 import Page2 from "./components/Page2";
-import Test1 from "./components/Test1";
-import Test2 from "./components/Test2";
-import First from "./components/First";
 
 function App() {
   return (
-    // <Routes>
-    //   <Route path="/" element={<Main />} />
-    //   <Route path="/p1" element={<Page1 />} />
-    //   <Route path="/p2" element={<Page2 />} />
-    // </Routes>
-
-    <Main />
+    <Routes>
+      <Route path="/" element={<Main />} />
+      <Route path="/p1" element={<Page1 />} />
+      <Route path="/p2" element={<Page2 />} />
+    </Routes>
   );
 }
 
